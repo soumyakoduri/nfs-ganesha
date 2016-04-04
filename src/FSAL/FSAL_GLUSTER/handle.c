d88047911668e160b67d7a2c295f42003afe5d55
Soumya Koduri <skoduri@redhat.com>
2016-04-04 18:31:50 +0530

    FSAL_GLUSTERL: add status2()
    Change-Id: Ic6d96ca404fa5303d0f325fdcd564cba00acea96
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -1814,12 +1814,14 @@ static fsal_status_t glusterfs_open2(struct fsal_obj_handle *obj_hdl,
 }
 
 /* status2
- * default case return 0
+ * return openflags of corresponding fd
  */
 
 static fsal_openflags_t glusterfs_status2(struct state_t *state)
 {
-	return 0;
+        struct glusterfs_fd *my_fd = (struct glusterfs_fd *)(state + 1);
+
+	return my_fd->openflags;
 }
 
 /* reopen2

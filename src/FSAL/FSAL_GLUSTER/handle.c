17f7f9b974fde90db512140e51308fbef34222ad
Soumya Koduri <skoduri@redhat.com>
2016-05-12 16:28:51 +0530

    FSAL_GLUSTER: implement status2 fop
    Given a fd, return its corresponding openflags.
    
    Change-Id: Idfd5be6038b70372d9d76b06a5d86560c2271fd7
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -2324,13 +2324,15 @@ direrr:
 }
 
 /* status2
- * default case return 0
+ * return openflags of corresponding fd
  */
 
 static fsal_openflags_t glusterfs_status2(struct fsal_obj_handle *obj_hdl,
 					  struct state_t *state)
 {
-	return 0;
+	struct glusterfs_fd *my_fd = (struct glusterfs_fd *)(state + 1);
+
+	return my_fd->openflags;
 }
 
 /* reopen2

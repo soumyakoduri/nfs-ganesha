b99ecaf2122aef50e4335041e1906e0138acc476
Soumya Koduri <skoduri@redhat.com>
2018-10-09 13:16:13 +0530

    FSAL_GLUSTER: Support new lease fop
    Change-Id: I95fefc2d6cab38aa45a45a4c76bb44530daecbda
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>

--- src/FSAL/FSAL_GLUSTER/gluster_internal.h
+++ src/FSAL/FSAL_GLUSTER/gluster_internal.h
@@ -182,6 +182,8 @@ struct glusterfs_handle {
 	uint64_t rw_issued;
 	uint64_t rw_serial;
 	uint64_t rw_max_len;
+	glfs_lease_types_t lease_type; /* Store lease_type granted to
+					 this file: NONE,RD or RW */
 };
 
 /* Structures defined for PNFS */
